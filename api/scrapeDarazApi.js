9110a3e4b49f958d5efeecd13f67128efe8d235a
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-01-10 19:44:38 +0545

    updated daraz api

--- api/scrapeDarazApi.js
+++ api/scrapeDarazApi.js
@@ -2,99 +2,42 @@ import puppeteer from 'puppeteer';
 
 export async function scrapeDarazProduct(productName) {
   try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox', '--start-maximized'],
-    });
-
+    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox', '--start-maximized'] });
     const page = await browser.newPage();
     await page.setViewport({ width: 2020, height: 3080 });
-    
-    const formattedProductName = productName.replace(/[:,-,_]/g, '');
-    const searchTerms = formattedProductName.split(/\s+/).filter(Boolean);
-    
-    // Create a case-insensitive regular expression with positive lookahead for each search term
-    const searchRegex = new RegExp(
-      `(?=.*${searchTerms.map(term => `\\b${term}\\b`).join(')(?=.*')})`,
-      'i'
-    );
-    
-    const singularForm = formattedProductName;
-    const pluralForm = formattedProductName.endsWith('s') ? formattedProductName : formattedProductName + 's';
-    
-    // Create a case-insensitive regular expression with a requirement of at least two common letters
-    const commonLettersRegex = new RegExp(`(?=.*[a-zA-Z].*[a-zA-Z])(${singularForm}|${pluralForm})`, 'i');
-    
+
     const encodedProductName = encodeURIComponent(productName);
     const url = `https://www.daraz.com.np/catalog/?q=${encodedProductName}`;
 
     await page.goto(url);
 
-    const areElementsPresent = await page.waitForSelector('.gridItem--Yd0sa', { visible: true, timeout: 4000 })
-      .then(() => true)
-      .catch(() => false);
-
-    if (!areElementsPresent) {
-      await browser.close();
-      return JSON.stringify({ message: 'An error occurred while scraping the product data. Required elements not found.' });
-    }
-
-    await page.waitForSelector('.gridItem--Yd0sa', { visible: true });
-
-    const products = await page.$$eval('.gridItem--Yd0sa', (productItems) => {
+    const products = await page.$$eval('[data-qa-locator="product-item"]', (productItems) => {
       return productItems.slice(0, 10).map((productItem) => {
-        const title = productItem.querySelector('.title--wFj93 a').textContent;
-        const priceElement = productItem.querySelector('.price--NVB62');
-        const price = priceElement ? priceElement.textContent.replace(/[^0-9]/g, '') : 'Price not found';     
-        const discountElement = productItem.querySelector('.discount--HADrg');
-        const discount = discountElement ? discountElement.textContent : 'No discount';
+        const title = productItem.querySelector('.title-wrapper--IaQ0m').textContent.trim();
+        const price = (productItem.querySelector('.current-price--Jklkc .currency--GVKjl') || {}).textContent || 'Price not found';
+        const discount = (productItem.querySelector('.original-price--lHYOH .currency--GVKjl') || {}).textContent || 'No discount';
+        const originalPrice = (productItem.querySelector('.original-price--lHYOH del.currency--GVKjl') || {}).textContent.trim() || '';
+        const imgSrc = (productItem.querySelector('.image--Smuib') || {}).getAttribute('src') || 'Image not found';
+        const url = productItem.querySelector('a.product-card--vHfY9').getAttribute('href') || 'URL not found';
 
-        const originalPriceElement = productItem.querySelector('.origPrice--AJxRs');
-        const actualPrice = originalPriceElement ? originalPriceElement.textContent.trim() : '';
-
-        const imgElement = productItem.querySelector('.image--Smuib');
-        const imgSrc = imgElement ? imgElement.getAttribute('src') : 'Image not found';
-
-        const urlElement = productItem.querySelector('.title--wFj93 a');
-        const url = urlElement ? urlElement.getAttribute('href') : 'URL not found';
-
-        return {
-          title,
-          price,
-          discount,
-          originalPrice: actualPrice,
-          img: imgSrc,
-          url,
-        };
+        return { title, price, discount, originalPrice, img: imgSrc, url };
       });
     });
 
-    // Filter out irrelevant products based on a case-insensitive match
-    const relevantProducts = products.filter((product) =>
-      searchRegex.test(product.title.toLowerCase()) && commonLettersRegex.test(product.title)
-    );
-// Sort the relevant products by price in ascending order
-relevantProducts.sort((a, b) => {
-  const priceA = parseFloat(a.price);
-  const priceB = parseFloat(b.price);
+    const relevantProducts = products.filter((product) => {
+      const titleLowerCase = product.title.toLowerCase();
+      return titleLowerCase.includes(productName.toLowerCase()) && /[a-zA-Z].*[a-zA-Z]/.test(product.title);
+    });
 
-  // Check if prices are valid numbers before comparison
-  if (!isNaN(priceA) && !isNaN(priceB)) {
-    return priceA - priceB;
-  } else {
-    // If either price is not a valid number, keep the current order
-    return 0;
-  }
-});
+    relevantProducts.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
 
     await browser.close();
 
-    if (relevantProducts.length === 0) {
-      return JSON.stringify({ message: 'No relevant products found' });
-    }
-
-    return relevantProducts;
+    return relevantProducts.length === 0
+      ? { message: 'No relevant products found' }
+      : relevantProducts;
   } catch (error) {
-    return JSON.stringify({ message: `An error occurred while scraping the product data: ${error.message}` });
+    return { message: `An error occurred while scraping the product data: ${error.message}` };
   }
 }
+
